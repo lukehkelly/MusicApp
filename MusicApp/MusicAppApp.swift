9810e62b6b6f3531c982c1f7261720c8607184a9
lukehkelly <lhkelly@unc.edu>
2025-11-18 18:25:49 -0500

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Luke Kelly on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
